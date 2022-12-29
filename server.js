24ab811507d1626310c732d421b03c08b5b1bb38
KhangDuong <khangdt10@fpt.com.vn>
2022-12-29 10:16:58 +0700

    layout

--- server.js
+++ server.js
@@ -2,12 +2,21 @@ require('dotenv').config()
 const express = require('express')
 const logger = require('morgan')
 const fs = require('fs')
-
+const path = require('path')
+var expressLayouts = require('express-ejs-layouts');
 const redis = require('./database/redis')
 const mongoose = require("./database/mongoose")
 
 const app = express()
 const port = process.env.APP_PORT || 3000
+
+app.set('view engine', 'ejs');
+
+app.use(expressLayouts);
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.set('layout', './layouts/master');
 
 app.use(express.static('public'))
 
@@ -23,4 +32,8 @@ mongoose.connect()
 
 app.listen(port, () => {
     console.log(`[+] NodeJS app running at localhost: ${port}!`)
+})
+
+app.get('/', (req, res) => {
+    return res.render('home/index')
 })
